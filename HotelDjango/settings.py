11f37cd1e592829977ce5823bbeb8bbca0e1bfbf
ma.minnahmetov <gtr425gtr@gmail.com>
2022-10-18 15:45:30 +0300

    settings

--- HotelDjango/settings.py
+++ HotelDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8qu^ilkxg5urk_-3ge$drsut#7&!)j5rkm^joy%i%^d83$!q!h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['176.126.103.130']
 
 
 # Application definition
