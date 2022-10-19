0dde2690d439193cd74e788a7a4b0c028fc930dc
ma.minnahmetov <gtr425gtr@gmail.com>
2022-10-19 16:07:53 +0300

    Timezone

--- Hotel/views.py
+++ Hotel/views.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 from constance import config
 from django.shortcuts import render
-from datetime import datetime
+from django.utils import timezone
 from plotly.offline import plot
 
 from Hotel.const import ROOM_ID_NAME_DICT, MESSHIGH_NAME_DICT
@@ -12,12 +12,9 @@ from Hotel.models import EventBRS, ActiveIntervalsBRS, Booking
 
 
 def index(request):
-    start_date = config.START_TIME
-    finish_date = config.FINISH_TIME
-
-    events_by_sensor_id = EventBRS.get_by_interval(start_date, finish_date)
+    events_by_sensor_id = EventBRS.get_by_interval(config.START_TIME, config.FINISH_TIME)
     intervals_by_sensor_id = ActiveIntervalsBRS.get_sensor_id_intervals_by_events_dict(events_by_sensor_id)
-    bookings_by_room_number = asyncio.run(Booking.get_by_interval(start_date, finish_date))
+    bookings_by_room_number = asyncio.run(Booking.get_by_interval(config.START_TIME, config.FINISH_TIME))
     plotly_tasks = []
     for bookings in bookings_by_room_number.values():
         for booking in bookings:
@@ -37,11 +34,15 @@ def index(request):
                 {'Task': f'{name}', 'Start': str(interval.start), 'Finish': str(interval.finish), 'Resource': 'БРС'}
             )
     discrete_map_resource = {'БРС': '#D52941', 'Travelline': '#FCD581'}
-    fig = px.timeline(pd.DataFrame(plotly_tasks), x_start="Start", x_end="Finish", y="Task", color='Resource', color_discrete_map=discrete_map_resource)
+    fig = px.timeline(pd.DataFrame(plotly_tasks),
+                      x_start="Start",
+                      x_end="Finish",
+                      y="Task",
+                      color='Resource',
+                      color_discrete_map=discrete_map_resource)
     fig.update_yaxes(autorange="reversed")
-    fig.update_layout(shapes=[{
-        'type': 'line', 'yref': 'paper', 'y0': 0, 'y1': 1, 'xref': 'x', 'x0': datetime.now(), 'x1': datetime.now()
-    }])
+    now = timezone.localtime(timezone.now())
+    fig.update_layout(shapes=[{'type': 'line', 'yref': 'paper', 'y0': 0, 'y1': 1, 'xref': 'x', 'x0': now, 'x1': now}])
     fig.update_layout({
         'plot_bgcolor': '#ECE7DC',
         # 'paper_bgcolor': '#FFFFF9',
